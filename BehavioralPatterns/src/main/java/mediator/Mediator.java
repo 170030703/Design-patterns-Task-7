847c8962a234dadd3d1ef99ef833170665974e46
170030703 <170030703@kluniversity.in>
2020-03-09 16:00:50 +0530

    Behavioral Pattern

---
+++ BehavioralPatterns/src/main/java/mediator/Mediator.java
@@ -0,0 +1,11 @@
+package mediator;
+
+public class Mediator {
+	   public static void main(String[] args) {
+	      User robert = new User("Robert");
+	      User john = new User("John");
+
+	      robert.sendMessage("Welcome to Epam!");
+	      john.sendMessage("Bhargav!");
+	   }
+	}
